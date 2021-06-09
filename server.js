6b993ab3bdcae75f62d02afc882a55fb221f0f5e
avneet5658 <avneet.pandey@mail.vinove.com>
2021-06-09 15:58:20 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { v4: uuuidV4 } = require("uuid");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuuidV4()}`);
+});
+app.get("/:roomId", (req, res) => {
+  res.render("room", { roomId: req.params.roomId });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
+  });
+  socket.on("disconnect", (roomId, userId) => {
+    socket.to(roomId).emit("user-disconnected", userId);
+  });
+});
+
+server.listen(3000);
